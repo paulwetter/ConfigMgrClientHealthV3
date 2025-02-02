2f487fad772404110177188e84f10bf6e9355ff8
Paul Wetter <tellwetter@gmail.com>
2025-02-02 02:33:07 -0600

    Updated API and Docs
    * Updated API Zip file
    * Commented entire sql script as API configures the database.
    * Readme updated.
    * Config, minor tweaks.

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,3 +1,7 @@
+---NOT NEEDED WITH LATEST V3---
+---latest v3 uses entity framework migrations to build the database tables. so this is no longer needed---
+
+
 -- SQL query to create and/or upgrade the database and tables for ConfigMgr Client Health
 
 -- IF CREATING DATABASE FOR FIRST TIME:
@@ -7,119 +11,119 @@
 
 -- START QUERY
 -- Create database if not exist:
-GO
-IF NOT EXISTS (SELECT [name] FROM sys.databases WHERE [name] = 'ClientHealth')
-CREATE DATABASE ClientHealth
+-- GO
+-- IF NOT EXISTS (SELECT [name] FROM sys.databases WHERE [name] = 'ClientHealth')
+-- CREATE DATABASE ClientHealth
 
-GO
-USE ClientHealth
+-- GO
+-- USE ClientHealth
 
--- Create Configuration table if not exist:
-GO
-IF NOT EXISTS (SELECT [name] FROM sys.tables WHERE [name] = 'Configuration')
-CREATE TABLE dbo.Configuration
-(
-    Name varchar(50) NOT NULL UNIQUE,
-    Version varchar (10) NOT NULL
-)
+-- -- Create Configuration table if not exist:
+-- GO
+-- IF NOT EXISTS (SELECT [name] FROM sys.tables WHERE [name] = 'Configuration')
+-- CREATE TABLE dbo.Configuration
+-- (
+--     Name varchar(50) NOT NULL UNIQUE,
+--     Version varchar (10) NOT NULL
+-- )
 
--- Create ClientConfiguration table if not exist:
-GO
-IF NOT EXISTS (SELECT [name] FROM sys.tables WHERE [name] = 'ClientConfiguration')
-CREATE TABLE dbo.ClientConfiguration
-(
-    Id varchar(100) NOT NULL PRIMARY KEY,
-    Configuration varchar(max) NOT NULL
-)
+-- -- Create ClientConfiguration table if not exist:
+-- GO
+-- IF NOT EXISTS (SELECT [name] FROM sys.tables WHERE [name] = 'ClientConfiguration')
+-- CREATE TABLE dbo.ClientConfiguration
+-- (
+--     Id varchar(100) NOT NULL PRIMARY KEY,
+--     Configuration varchar(max) NOT NULL
+-- )
 
--- Create Clients table if not exist:
-IF NOT EXISTS (SELECT [name] FROM sys.tables WHERE [name] = 'Clients')
-CREATE TABLE dbo.Clients
-(
-    ClientHealthId varchar(36) NOT NULL PRIMARY KEY,
-    Hostname varchar(MAX) NOT NULL,
-    OperatingSystem varchar(MAX) NOT NULL,
-    Architecture varchar(10) NOT NULL,
-    Build varchar(MAX) NOT NULL,
-    Manufacturer varchar(MAX),
-    Model varchar(MAX),
-    InstallDate smalldatetime,
-    OSUpdates smalldatetime,
-    LastLoggedOnUser varchar(MAX),
-    ClientVersion varchar(20),
-    PSVersion float,
-    PSBuild int,
-    Sitecode varchar(3),
-    Domain varchar(MAX),
-    MaxLogSize int,
-    MaxLogHistory int,
-    CacheSize int,
-    ClientCertificate varchar(MAX),
-    ProvisioningMode varchar(MAX),
-    DNS varchar(MAX),
-    Drivers varchar(max),
-    Updates varchar(MAX),
-    PendingReboot varchar(MAX),
-    LastBootTime smalldatetime,
-    OSDiskFreeSpace float,
-    Services varchar(max),
-    AdminShare varchar(MAX),
-    StateMessages varchar(MAX),
-    WUAHandler varchar(MAX),
-    WMI varchar(MAX),
-    RefreshComplianceState smalldatetime,
-    ClientInstalled smalldatetime,
-    Version varchar(10),
-    Timestamp datetime,
-    HWInventory smalldatetime,
-    SWMetering varchar(MAX),
-    BITS varchar(MAX),
-    PatchLevel int,
-    ClientInstalledReason varchar(max),
-    Extension_000 varchar(max),
-    Extension_001 varchar(max),
-    Extension_002 varchar(max),
-    Extension_003 varchar(max),
-    Extension_004 varchar(max),
-    Extension_005 varchar(max),
-    Extension_006 varchar(max),
-    Extension_007 varchar(max),
-    Extension_008 varchar(max),
-    Extension_009 varchar(max),
-    Extension_010 varchar(max),
-    Extension_011 varchar(max),
-    Extension_012 varchar(max),
-    Extension_013 varchar(max),
-    Extension_014 varchar(max),
-    Extension_015 varchar(max),
-    Extension_016 varchar(max),
-    Extension_017 varchar(max),
-    Extension_018 varchar(max),
-    Extension_019 varchar(max)
-)
---else
+-- -- Create Clients table if not exist:
+-- IF NOT EXISTS (SELECT [name] FROM sys.tables WHERE [name] = 'Clients')
+-- CREATE TABLE dbo.Clients
+-- (
+--     ClientHealthId varchar(36) NOT NULL PRIMARY KEY,
+--     Hostname varchar(MAX) NOT NULL,
+--     OperatingSystem varchar(MAX) NOT NULL,
+--     Architecture varchar(10) NOT NULL,
+--     Build varchar(MAX) NOT NULL,
+--     Manufacturer varchar(MAX),
+--     Model varchar(MAX),
+--     InstallDate smalldatetime,
+--     OSUpdates smalldatetime,
+--     LastLoggedOnUser varchar(MAX),
+--     ClientVersion varchar(20),
+--     PSVersion float,
+--     PSBuild int,
+--     Sitecode varchar(3),
+--     Domain varchar(MAX),
+--     MaxLogSize int,
+--     MaxLogHistory int,
+--     CacheSize int,
+--     ClientCertificate varchar(MAX),
+--     ProvisioningMode varchar(MAX),
+--     DNS varchar(MAX),
+--     Drivers varchar(max),
+--     Updates varchar(MAX),
+--     PendingReboot varchar(MAX),
+--     LastBootTime smalldatetime,
+--     OSDiskFreeSpace float,
+--     Services varchar(max),
+--     AdminShare varchar(MAX),
+--     StateMessages varchar(MAX),
+--     WUAHandler varchar(MAX),
+--     WMI varchar(MAX),
+--     RefreshComplianceState smalldatetime,
+--     ClientInstalled smalldatetime,
+--     Version varchar(10),
+--     Timestamp datetime,
+--     HWInventory smalldatetime,
+--     SWMetering varchar(MAX),
+--     BITS varchar(MAX),
+--     PatchLevel int,
+--     ClientInstalledReason varchar(max),
+--     Extension_000 varchar(max),
+--     Extension_001 varchar(max),
+--     Extension_002 varchar(max),
+--     Extension_003 varchar(max),
+--     Extension_004 varchar(max),
+--     Extension_005 varchar(max),
+--     Extension_006 varchar(max),
+--     Extension_007 varchar(max),
+--     Extension_008 varchar(max),
+--     Extension_009 varchar(max),
+--     Extension_010 varchar(max),
+--     Extension_011 varchar(max),
+--     Extension_012 varchar(max),
+--     Extension_013 varchar(max),
+--     Extension_014 varchar(max),
+--     Extension_015 varchar(max),
+--     Extension_016 varchar(max),
+--     Extension_017 varchar(max),
+--     Extension_018 varchar(max),
+--     Extension_019 varchar(max)
+-- )
+-- --else
 
--- START Changes to database --
--- Add columns if needed
---IF NOT EXISTS (SELECT * FROM sys.columns WHERE  object_id = OBJECT_ID(N'[dbo].[Clients]') AND name = 'HWInventory') ALTER TABLE dbo.Clients ADD HWInventory smalldatetime
+-- -- START Changes to database --
+-- -- Add columns if needed
+-- --IF NOT EXISTS (SELECT * FROM sys.columns WHERE  object_id = OBJECT_ID(N'[dbo].[Clients]') AND name = 'HWInventory') ALTER TABLE dbo.Clients ADD HWInventory smalldatetime
 
 
--- Modify columns if needed
---IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Clients' AND COLUMN_NAME = 'Hostname' AND DATA_TYPE = 'varchar' AND CHARACTER_MAXIMUM_LENGTH = 100) ALTER TABLE dbo.Clients ALTER COLUMN Hostname varchar(MAX) NOT NULL
+-- -- Modify columns if needed
+-- --IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Clients' AND COLUMN_NAME = 'Hostname' AND DATA_TYPE = 'varchar' AND CHARACTER_MAXIMUM_LENGTH = 100) ALTER TABLE dbo.Clients ALTER COLUMN Hostname varchar(MAX) NOT NULL
 
--- Set latest ConfigMgr Client Health database version:
-GO
-begin tran
-if exists (SELECT * FROM dbo.Configuration WITH (updlock,serializable) WHERE Name='ClientHealth')
-begin
-    IF EXISTS (SELECT * FROM dbo.Configuration WITH (updlock,serializable) WHERE Name='ClientHealth' AND Version < '2.0.0')
-    UPDATE dbo.Configuration SET Version='2.0.0' WHERE Name = 'ClientHealth'
-end
-else
-begin
-    INSERT INTO dbo.Configuration (Name, Version)
-    VALUES ('ClientHealth', '2.0.0')
-end
-commit tran
+-- -- Set latest ConfigMgr Client Health database version:
+-- GO
+-- begin tran
+-- if exists (SELECT * FROM dbo.Configuration WITH (updlock,serializable) WHERE Name='ClientHealth')
+-- begin
+--     IF EXISTS (SELECT * FROM dbo.Configuration WITH (updlock,serializable) WHERE Name='ClientHealth' AND Version < '2.0.0')
+--     UPDATE dbo.Configuration SET Version='2.0.0' WHERE Name = 'ClientHealth'
+-- end
+-- else
+-- begin
+--     INSERT INTO dbo.Configuration (Name, Version)
+--     VALUES ('ClientHealth', '2.0.0')
+-- end
+-- commit tran
 
 -- End of query
--- README.md
+++ README.md
@@ -1,16 +1,77 @@
-# ConfigMgr Client Health
+# ConfigMgr Client Health 3.x
 
 Version: 3.0.0 alpha
 
 __WARNING:__ Not fully functional yet.  Still needs some work.
 
-This is the master branch of ConfigMgr Client Health and is ready for production.
+Verision 3 of the API uses entity framework to build the database.  The api is built on .net 8.  The api can serve the configuration for the client as well through the endpoint.
+
+## API Endpoints:
+
+### Get
+
+* /api/Clients/ClientConfiguration
+* /api/Clients/Client
+
+### Put
+
+* /api/Clients/ClientConfiguration
+* /api/Clients/Client
+
+## Installation of 3.x
+
+### API Install
+
+1. Create `ClientHealth` database on your database server.
+1. Install IIS with default options on server that will host API.
+1. Install [.Net 8 Hosting bundle](https://dotnet.microsoft.com/en-us/download/dotnet/8.0) on server.
+1. Create a new Website in IIS with a custom port of your choice.
+1. Copy the [ClientHealthWebServiceV3.zip](./ClientHealthWebServiceV3.zip) file to the server.
+1. Extract the files from the zip onto the root of the new IIS website.
+1. Once Extracted, open `appsettings.json` in your favorite text editor.
+   * On line 3, update the `DefaultConnection` string with your database name and a username and password.
+   * At the end of the file, set the `EnableSwagger` line to `true`.
+   * Note the `ClientApiKeys`.  Update the key to a password/secret you will use for reading configurations and writing the client health to the database.
+   * Note the `ConfigurationApiKeys`.  Update the key to a password/secret you will use for writing configurations to the database.  This should be kept more secret than the `ClientApiKeys` as the configuration key will only be used to write the client configuration. (I'll probably eventually write a powershell script to use the endpoint to modify the configuration more easily.)
+1. Restart the website.
+1. Review log file.  It should have applied migrations.
+1. Check the database.  It should now contain a `Clients` and `ClientConfiguration` table.
+1. Browse to https://server.domain.com:7107/swagger.  Adjust this url to your iis server and port.
+
+### API Configuration
+
+1. Browse to https://server.domain.com:7107/swagger.
+1. Take a copy of the [config.json](./config.json).
+1. Make your adjustments to the configuration.
+1. On the Swagger page, open the `/api/Clients/ClientConfiguration` __PUT__ endpoint.
+1. Click "Try it out".
+1. Enter the `ConfigurationApiKeys` in the __ApiKey__ field.
+1. in the "Request body", copy the entire "config.json" text into the field and click `Execute`.
+   * This will write the configuration to the `ClientConfiguration` table in the database and can then be read by the clients.
+
+### Clienthealth Script Install and Configuration
+
+Client Health script can be copied to each of your clients in various methods either Group Policy, Configuration Manager, Intune, Etc. Below are general guidelines on how to install it.
+
+The config file from the old script has been changed to json.  The configuration settings are generally the same as the old.
+
+* You will have to copy the `ConfigMgrClientHealth.ps1` and the `config.json` that you copied and modified from above.
+* "Install" these on all of your PCs you want to monitor health on.
+* Install location would be recommended at `C:\ProgramData\ClientHealth`.
+* Create a scheduled task that will run on a daily schedule.  The command line will look something like this:
+
+```powershell
+C:\ProgramData\ClientHealth\ConfigMgrClientHealth.ps1 -Config C:\ProgramData\ClientHealth\Config.json
+```
+
+The above are the basics of the v3.  the V3 client health script when configured to get the config from the API will be able to update the config from the API.
+
+
+## Original Change log from 2.x
 
 [ConfigMgr Client Health Full documentation](https://www.andersrodland.com/configmgr-client-health/)
 
-
-
-## Changes since stable release
+### Changes since stable release
 
 * Client Health now successfully sets the client max log history.
 * Client Health now successfully sets the client cache size.
